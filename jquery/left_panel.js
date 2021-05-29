8c5cf22542450f6ff05a865e518ae687c6ddba0d
Krzysztof <krzysaaa7@gmail.com>
2021-05-30 00:47:45 +0200

    html in html

--- jquery/left_panel.js
+++ jquery/left_panel.js
@@ -1,6 +1,5 @@
 $(document).ready(function() {
     $("button.left_panel_button").click(function() {
-        $("#main-game-panel").html('<h1>' + $(this).text()+ '</h1>');
-        console.log("siema");
+        $("#main-game-panel").load($(this).val() + ".html");
     })
 });
